7ffd60079f133e774331a663ea439b7f62958441
Aarav <aarav.rajput2003@gmail.com>
2024-05-15 09:45:23 +0530

    better frontend

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
 import subprocess
 import os
 from flask import Flask, request, render_template, jsonify
+from flask_cors import CORS
+from flask_dropzone import Dropzone
 
 app = Flask(__name__)
+CORS(app)
+
+app.config.update(
+    DROPZONE_ALLOWED_FILE_CUSTOM=True,
+    DROPZONE_ALLOWED_FILE_TYPE='.c,.cpp',
+    DROPZONE_MAX_FILE_SIZE=3024,
+    DROPZONE_MAX_FILES=1,
+    DROPZONE_IN_FORM=True,
+    DROPZONE_UPLOAD_ON_CLICK=True,
+    DROPZONE_UPLOAD_ACTION='scan',
+    DROPZONE_TIMEOUT=5*60*1000
+)
+
+dropzone = Dropzone(app)
 
 @app.route('/')
 def index():
@@ -10,16 +26,24 @@ def index():
 
 @app.route('/scan', methods=['POST'])
 def scan():
-    filepath = request.form.get('filepath')
+
+    print(request.form)
+
+    filename = request.form.get('fileName')
     code = request.form.get('code')
+
+    filepath = os.path.join(os.path.dirname(__file__), filename)
+
     python_path = "python"
+
+    error_log = []
 
     if code:
         try:
             with open(filepath, 'w') as f:
                 f.write(code)
         except Exception as e:
-            return f"Error saving code to file: {e}"
+            error_log.append(f"Error during file creation: {e}")
 
     try:
         meowlinter_path = os.path.join(os.path.dirname(__file__), 'meowlinter.py')
@@ -28,12 +52,14 @@ def scan():
         output, error = process.communicate()
 
         if error:
-            return f"Error during scan: {error.decode('utf-8')}"
+            error_log.append(f"Error during scan: {error}")
 
-        return output.decode('utf-8')
+        result = output.decode('utf-8')
 
     except Exception as e:
-        return f"Error during scan: {e}"
+        error_log.append(f"Error during scan: {e}")
+
+    return render_template('result.html', result=result, error_log=error_log)
 
 @app.route('/check_file')
 def check_file():
@@ -42,4 +68,4 @@ def check_file():
     return jsonify({'exists': file_exists})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0',port=80)
